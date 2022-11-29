60b1e633bd592f73950d4328f7553677a66ca636
Mashbrow <maxence.nesme@live.fr>
2022-11-29 10:53:53 +0100

    Doc

--- team-api/src/app.ts
+++ team-api/src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from '../swagger.json';
 import * as routes from './routes'
 
 const app = express();
 app.use(express.json())
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 routes.register(app)
 
 export {app};
